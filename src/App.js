6cf8ae842682252a8a08db05a34cd56105bbd0bc
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2022-11-28 23:06:25 +0400

    Fix with eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-import Header from "./Header";
-import Skills from "./Skills";
+import './App.css';
+import Header from './Header';
+import Skills from './Skills';
 
 function App() {
   return (
+    /* eslint-disable */
     <div className="App">
       <Header />
       <Skills />
