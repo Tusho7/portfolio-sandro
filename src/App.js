479b286303a16d0664b418f7d3a8c14cf35771f2
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2022-11-29 15:35:56 +0400

    Add Projects Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
-import Header from './Header';
-import Skills from './Skills';
+import "./App.css";
+import Header from "./Header";
+import Projects from "./Projects";
+import Skills from "./Skills";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Skills />
+      <Projects />
     </div>
   );
 }
